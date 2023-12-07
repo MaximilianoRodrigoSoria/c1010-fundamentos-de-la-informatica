f80b37f848204d52e3596be6f4bbb2f80538498c
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2023-12-07 07:15:03 -0300

    Se agregan el resto de test

--- 02_practicas/33_ejercicio.py
+++ 02_practicas/33_ejercicio.py
@@ -3,6 +3,5 @@ email = input("Ingrese su dirección de correo electrónico: ")
 # Se puede usar una listata tambien
 usuario, dominio = email.split('@')
 
-
 print(f"Usuario: {usuario}")
 print(f"Dominio: {dominio}")
